7d3fdb780ae9cff21c45b4b0688231325b85692a
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2018-07-01 13:38:54 -0400

    Working on file I/O. Added compiler flag to makefile.

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,68 @@
 #include "Process.h"
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <array>
+
+std::vector<std::array<int, 3>> readInputFile(const std::string&);
 
 // Expected args: input filename, type of scheduler
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	std::cout << "Hello world\n";
+	std::vector<std::array<int, 3>> fileContents = readInputFile("test/00.txt");
+
+	for(const auto a : fileContents)
+	{
+		for(const auto b : a)
+		{
+			std::cout << b << "\t";
+		}
+
+		std::cout << "\n";
+	}
+
+	return 0;
+}
+
+std::vector<std::array<int, 3>> readInputFile(const std::string& fileName)
+{
+	std::vector<std::array<int, 3>> contents;
+	std::ifstream file;
+	file.open(fileName);
+	
+	if (file.is_open())
+	{
+		std::string item;
+		
+		int index = 0;
+
+		while(getline(file, item, '\t'))
+		{
+			std::array<int, 3> arr;
+
+			std::stringstream ss(item);
+			int x;
+			ss >> x;
+			
+			if(index == 2)
+			{
+				contents.push_back(arr);
+			}
+
+			arr[index] = x;
+
+			index = (index+1) % 3;
+		}
+
+		file.close();
+	}
+
+	else
+	{
+		std::cout << "Could not open file" << fileName << "\n";
+	}
+
+	return contents;
 }
--- Makefile
+++ Makefile
@@ -1,13 +1,13 @@
 all: simulator
 	
 simulator: process main
-	g++ Process.o Main.o -o simulator
+	g++ Process.o Main.o -o simulator -std=c++11
 
 process: Process.h Process.cpp
-	g++ -c Process.h Process.cpp
+	g++ -c Process.h Process.cpp -std=c++11
 
 main: Main.cpp
-	g++ -c Main.cpp
+	g++ -c Main.cpp -std=c++11
 
 clean:
 	rm *o simulator
